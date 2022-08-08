aae65fc5f96c9ee24f3a2b4cafea18148b9420e1
Eseoghene <efekodoeseoghene@gmail.com>
2022-08-08 15:32:25 +0100

    day 5!

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,38 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+require("@nomicfoundation/hardhat-chai-matchers");
+
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
+
+// module.exports = {
+//   solidity: "0.8.4",
+//   networks: {
+//     hardhat: {
+//       chainId: 1337,
+//     },
+//     mumbai: {
+//       url: process.env.STAGING_INFURA_URL,
+//       accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+//       gas: 2100000,
+//       gasPrice: 8000000000,
+//     },
+//   },
+// };
